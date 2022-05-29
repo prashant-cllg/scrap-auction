b8cf25e9882e1c13dbfee1aebd4d808951b3cdcc
Prashant Vishwakarma <prashant.cllg@gmail.com>
2022-05-29 15:22:36 +0530

    Automatic configuration with django-heroku

--- djangoprj/settings.py
+++ djangoprj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,3 +137,4 @@ STATIC_URL = '/static/'
 #configure media folder
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+django_heroku.settings(locals())
